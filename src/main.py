ccfde28a50cc29ad88aee1ae92a0dc6342aa1e62
AnotherCreator <jreginaldo@protonmail.com>
2023-01-10 18:31:38 -0600

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+"""
+
+
+Author: Josh Reginaldo
+"""
+
+def question_one():
+
+
+def question_two():
+
+
+def main():
+    question_one()
+    question_two()
+
+
+if __name__ == '__main__':
+    main()
