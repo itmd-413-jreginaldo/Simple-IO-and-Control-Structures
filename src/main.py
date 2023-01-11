0b26d58b99b7ba51133818e2051e6de657028534
AnotherCreator <jreginaldo@protonmail.com>
2023-01-10 18:43:50 -0600

    Add program description

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 """
+Simple IO and Control Structures
+
+This program will explore the control flow concepts in Python through the questions provided in the homework assignment.
+
+Question 1:
+Using the formula for the area of a regular polygon, the program will take the length and sides of a polygon inputted by
+the user. Using the input data, correctly output the total area to 4 decimal points.
+
+Question 2:
+Using the chart provided in the homework assignment, accurately create a program to replicate the
+"Troubleshooting Chart for Diesel Engines."
+
+The program will be able to restart continually until the user chooses to exit.
 
 
 Author: Josh Reginaldo
+GitHub: https://github.com/itmd-413-jreginaldo/Simple-IO-and-Control-Structures
 """
 
 def question_one():
