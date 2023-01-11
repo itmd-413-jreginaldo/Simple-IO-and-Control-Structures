73282ccbf7150d22e500301fc58c66ac0a74dffd
AnotherCreator <jreginaldo@protonmail.com>
2023-01-10 20:38:50 -0600

    Add question two functionality

--- src/main.py
+++ src/main.py
@@ -37,18 +37,83 @@ def question_one():
 
 def question_two():
     # Loop control value
-    user_program_choice = "Y"
+    user_program_choice = "y"
 
     # Loop will start as "True" in order for the program to run on start
-    while user_program_choice == "Y":
+    while user_program_choice == "y":
         print(f"\n=================================\n"
               f"DIESEL ENGINE TROUBLESHOOTING GUIDE"
               f"\n=================================\n")
 
-        
+        """
+        Attempt to get status light indicator
+        """
+        status_light = 0
+        while status_light != 1 and status_light != 2 and status_light != 3:
+            status_light = int(input(
+                "What color is the status light?"
+                "\n1: Green"
+                "\n2: Red"
+                "\n3: Amber"
+                "\nEnter corresponding number > "
+            ))
+
+        def switch_red_status_light():
+            print("\nShut off all input lines.")
+            meter_three_value = float(input("\nWhat is the value displayed on 'Meter 3?' > "))
+
+            if meter_three_value < 50:
+                print("\nCheck main line for test pressure.")
+                test_pressure_value = 0
+                while test_pressure_value != 1 and test_pressure_value != 2:
+                    test_pressure_value = int(input(
+                        "\nWhat is the test pressure value?"
+                        "\n1: Normal"
+                        "\n2: High"
+                        "\n3: Low"
+                        "\nEnter corresponding number > "
+                    ))
+
+                match test_pressure_value:
+                    case 1:
+                        print("\nRefer to motor service manual")
+                    case 2:
+                        print("\nRefer to main line manual")
+                    case 3:
+                        print("\nRefer to main line manual")
+            else:
+                print("\nMeasure flow velocity at inlet 2-B.")
+                flow_velocity = 0
+                while flow_velocity != 1 and flow_velocity != 2:
+                    flow_velocity = int(input(
+                        "\nWhat is the flow velocity value at 'Inlet 2-B?'"
+                        "\n1: Normal"
+                        "\n2: High"
+                        "\n3: Low"
+                        "\nEnter corresponding number > "
+                    ))
+
+                match flow_velocity:
+                    case 1:
+                        print("\nRefer to inlet service manual")
+                    case 2:
+                        print("\nRefer unit for factory service")
+                    case 3:
+                        print("\nRefer unit for factory service")
+
+        # Using status_light, switch to appropriate result
+        match status_light:
+            case 1:
+                print("\nDo restart procedure")
+            case 2:
+                switch_red_status_light()
+            case 3:
+                print("\nCheck fuel line service routine")
 
 
-        user_program_choice = input("Would you like to restart the guide? (Y/N)> ")
+        # Automatically set user input to lowercase to handle capital inputs
+        user_program_choice = str.lower(input("\nTo restart the guide, enter 'Y': "))
+
 
 def main():
     # question_one()
