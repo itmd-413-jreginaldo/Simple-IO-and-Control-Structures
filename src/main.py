d826d0f5815bc5cc9a82d5885e4abeff14395f40
AnotherCreator <jreginaldo@protonmail.com>
2023-01-10 19:42:00 -0600

    Add question one functionality

--- src/main.py
+++ src/main.py
@@ -18,14 +18,40 @@ Author: Josh Reginaldo
 GitHub: https://github.com/itmd-413-jreginaldo/Simple-IO-and-Control-Structures
 """
 
+import math
+
 def question_one():
+    # Get user input and convert string input to appropriate data type
+    polygon_sides = int(input("Enter the number of sides: "))
+    polygon_side_length = float(input("Enter the length of a side: "))
+
+    """
+    Polygon Area Formula:
+    
+    Area = (polygon_sides * polygon_side_length^2) / (4 * tan(pi / polygon_sides))
+    """
+    polygon_area = (polygon_sides * math.pow(polygon_side_length, 2)) / (4 * (math.tan((math.pi / polygon_sides))))
+
+    print(f"The area of the polygon is: {polygon_area:.4f}")
 
 
 def question_two():
+    # Loop control value
+    user_program_choice = "Y"
 
+    # Loop will start as "True" in order for the program to run on start
+    while user_program_choice == "Y":
+        print(f"\n=================================\n"
+              f"DIESEL ENGINE TROUBLESHOOTING GUIDE"
+              f"\n=================================\n")
+
+        
+
+
+        user_program_choice = input("Would you like to restart the guide? (Y/N)> ")
 
 def main():
-    question_one()
+    # question_one()
     question_two()
 
 
